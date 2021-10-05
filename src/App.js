2a2cafb6ec302a70fb613fcb09ff356bb84ed2af
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-05 20:38:11 +0300

    Сlean up project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import './App.css'
-import HeaderMenu from './Header/Header'
 import React from 'react'
-import Game from './Game/Game'
-import Transfer from './Transfer/Transfer'
-import Home from './Home/Home'
-import CreatingTeam from './CreatingTeam/CreatingTeam'
-import Registration from './Registration/Registration'
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
-	return (
-		<div>
-		<Router>
-			<HeaderMenu />
-			<Switch>
-				<Route exact path="/" component={Home}/>
-				<Route exact path="/game" component={Game}/>                   
-				<Route exact path="/transfer" component={Transfer}/>
-				<Route exact path="/creatingteam" component={CreatingTeam}/>
-				<Route exact path="/buy" component={CreatingTeam}/>
-				<Route exact path="/registration" component={Registration}/>
-			</Switch>
-		</Router>
-		</div>
-	);
+	return <></>
 }
 
-export default App;
+export default App
