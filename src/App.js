28b543b555277f97dbcc63063c297c5485f43a5f
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-06 21:19:07 +0300

    Installation and verification  Bootstrap.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import './App.css'
 import React from 'react'
 
 function App() {
-	return <>ааа</>
+	return (
+		<>
+			<div class="container">
+				<h1>How to Install Bootstrap in React App - ItSolutionStuff.com</h1>
+
+				<button class="btn btn-success">Success Button!</button>
+
+				<button class="btn btn-primary">Primary Button!</button>
+
+				<button class="btn btn-danger">Danger Button!</button>
+
+				<div class="alert alert-success">
+					<p>This is success alert.</p>
+				</div>
+
+				<div class="alert alert-primary">
+					<p>This is primary alert.</p>
+				</div>
+
+				<div class="alert alert-danger">
+					<p>This is danger alert.</p>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default App
@@ -19,3 +43,4 @@ export default App
 // npm install react-redux --save
 // npm install react-redux --save
 // npm install redux-thunk --save
+// npm install --save bootstrap
