6eb90d3ade87686f8a4f86d0ed325e2ce0268a26
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-06 20:34:14 +0300

    Add AddItem component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import './App.css'
 import React from 'react'
 
 function App() {
-	return <></>
+	return <>ааа</>
 }
 
 export default App
+
+// start of a new project
+// npm install -g json-server
+// npx json-server --watch db.json (server start)
+// npx create-react-app name (creating)
+// cd name
+// npm run start - (start program)
+// npm install react-router react-router-dom --save
+// import { useHistory } from 'react-router-dom'  (history)
+// npm install react-devtools --save
+// npm install react-redux --save
+// npm install react-redux --save
+// npm install redux-thunk --save
