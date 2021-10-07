326a7dddb1317c52d810ec425439025e660898c0
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-07 21:04:23 +0300

    Added Pages (Editor) folder

--- src/App.js
+++ src/App.js
@@ -2,31 +2,7 @@ import './App.css'
 import React from 'react'
 
 function App() {
-	return (
-		<>
-			<div class="container">
-				<h1>How to Install Bootstrap in React App - ItSolutionStuff.com</h1>
-
-				<button class="btn btn-success">Success Button!</button>
-
-				<button class="btn btn-primary">Primary Button!</button>
-
-				<button class="btn btn-danger">Danger Button!</button>
-
-				<div class="alert alert-success">
-					<p>This is success alert.</p>
-				</div>
-
-				<div class="alert alert-primary">
-					<p>This is primary alert.</p>
-				</div>
-
-				<div class="alert alert-danger">
-					<p>This is danger alert.</p>
-				</div>
-			</div>
-		</>
-	)
+	return <></>
 }
 
 export default App
