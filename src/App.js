986572bb77750320bcaafe043dab0bbe3ce45fad
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-07 21:08:55 +0300

    Added Pages (Todos) folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import './App.css'
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Router } from './pages/Router'
+import { Nav } from './pages/Nav'
+import './App.css'
+
+const navItems = [
+	{
+		href: '/todos',
+		title: 'Todos',
+	},
+	{
+		href: '/editor',
+		title: 'Editor',
+	},
+]
 
 function App() {
-	return <></>
+	return (
+		<div className="app__container">
+			<Router>
+				<Nav activeKey="/todos" items={navItems} />
+			</Router>
+		</div>
+	)
 }
 
 export default App
