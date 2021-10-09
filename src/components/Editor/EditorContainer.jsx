cd8d80ef17958532afe8bd9604baa41a7ea4a374
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-09 22:02:35 +0300

    Implemented execution, deletion, change of elements ToDo.

--- src/components/Editor/EditorContainer.jsx
+++ src/components/Editor/EditorContainer.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
 import { Editor } from './Editor'
 import { useSelector, useDispatch } from 'react-redux'
-import {} from '../../store/todo-actions'
+import {
+	UPDATE_TODO_TEXT,
+	UPDATE_TODO_DONE,
+	DELETE_TODO,
+} from '../../store/todo-actions'
 
 export function EditorContainer() {
 	const dispatch = useDispatch()
 	const items = useSelector(store => store.todos)
-	const handleTextChange = (item, value) => {
-		console.log(item, value)
+	const handleTextChange = (item, text) => {
+		dispatch({
+			type: UPDATE_TODO_TEXT,
+			payload: {
+				id: item.id,
+				text,
+			},
+		})
 	}
 
-	const handleToggle = (item, value) => {
-		console.log(item, value)
+	const handleToggle = (item, done) => {
+		dispatch({
+			type: UPDATE_TODO_DONE,
+			payload: {
+				id: item.id,
+				done,
+			},
+		})
 	}
-
 	const handleRemove = item => {
-		console.log(item)
+		dispatch({
+			type: DELETE_TODO,
+			payload: {
+				id: item.id,
+			},
+		})
 	}
 
 	return (
