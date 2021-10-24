6a0906f8ae2509c727498d35d6c654e8e23287c4
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-10-24 21:27:08 +0300

    Add get todos api

--- src/components/Additem/AddItem.jsx
+++ src/components/Additem/AddItem.jsx
@@ -6,35 +6,35 @@ import { Plus } from 'react-bootstrap-icons'
 import './AddItem.css'
 
 export function AddItem({ onAdd }) {
-  const [text, setText] = useState('')
+	const [text, setText] = useState('')
 
-  const handleChange = (value) => {
-    setText(value)
-  }
+	const handleChange = value => {
+		setText(value)
+	}
 
-  const handleAdd = () => {
-    onAdd(text)
-    setText('')
-  }
+	const handleAdd = () => {
+		onAdd(text)
+		setText('')
+	}
 
-  return (
-    <div className="add-item__container">
-      <Form.Group className="add-item__item">
-        <Form.Control
-          type="text"
-          className="add-item__input"
-          value={text}
-          onChange={(event) => handleChange(event.target.value)}
-        />
-        <Button
-          type="button"
-          variant="primary"
-          className="add-item__button"
-          onClick={handleAdd}
-        >
-          <Plus />
-        </Button>
-      </Form.Group>
-    </div>
-  )
+	return (
+		<div className="add-item__container">
+			<Form.Group className="add-item__item">
+				<Form.Control
+					type="text"
+					className="add-item__input"
+					value={text}
+					onChange={event => handleChange(event.target.value)}
+				/>
+				<Button
+					type="button"
+					variant="primary"
+					className="add-item__button"
+					onClick={handleAdd}
+				>
+					<Plus />
+				</Button>
+			</Form.Group>
+		</div>
+	)
 }
